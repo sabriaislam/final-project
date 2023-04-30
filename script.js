bd72015cef9a670b921b9628040345840065614f
Sabria Islamx <sabria.islam@gmail.com>
2023-04-29 23:52:17 -0400

    rough first page

---
+++ script.js
@@ -0,0 +1,32 @@
+function displayImages() {
+  const imageUrls = [    "content/gif-1.gif",    "content/gif-2.gif",    "content/gif-3.gif",    "content/gif-4.gif",    "content/gif-5.gif",    "content/gif-6.gif"  ];
+
+  // shuffle the array randomly
+  for (let i = imageUrls.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [imageUrls[i], imageUrls[j]] = [imageUrls[j], imageUrls[i]];
+  }
+
+  const container = document.getElementById("content");
+
+  // loop through the array three times
+  for (let j = 0; j < 12; j++) {
+    // shuffle the array randomly each time
+    for (let i = imageUrls.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [imageUrls[i], imageUrls[j]] = [imageUrls[j], imageUrls[i]];
+    }
+    // display the shuffled images
+    for (let i = 0; i < imageUrls.length; i++) {
+      const image = document.createElement("img");
+      image.src = imageUrls[i];
+      image.classList.add("bg-gif");
+      container.appendChild(image);
+    }
+  }
+}
+
+displayImages();
+
+
+
